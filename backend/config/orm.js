d9d92cb23a540fd6d0b2e37ad562418283727f87
Paul Rogers <paulwrogers@yahoo.com>
2019-02-04 11:12:11 -0800

    small update

--- backend/config/orm.js
+++ backend/config/orm.js
@@ -1,10 +1,8 @@
-
-
 var connection = require("../config/connection.js");
 
 var orm = {
 
-    getAll: function (cb) {
+        getAll: function (cb) {
         var queryString = "SELECT * FROM CURRENT_RENTS"
         connection.query(queryString, function (err, result) {
             if (err) {
@@ -12,6 +10,18 @@ var orm = {
             }
             cb(result);
         });
-    }};
+    },
+        addUserData: function (cb) {
+            var queryString = "INSERT INTO USER_DATA VALUES ?"
+            connection.query(queryString, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                cb(result);
+            });
+        }
+    };
+
+
 
     module.exports = orm;

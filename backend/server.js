f13e54ec5106f3f47ba9c75de790c4796d154d3a
Pete Widders <pwidders@hotmail.com>
2019-02-06 13:16:00 -0800

    Sequelize DB setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 var axios = require('axios');
 var cors = require('cors');
-var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
+var Sequelize = require('Sequelize');
+
+// Requiring our models for syncing
+var db = require('./models');
+
+// Creates connection with Sequelize
+const connection = new Sequelize('live_work_chill_db', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+});
 
 // Create instance of cors with Express
 app.use(cors());
@@ -15,7 +25,9 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
-// Express server listen
-app.listen(port, function() {
-    console.log("Server is running on Port: " + port);
-})
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});

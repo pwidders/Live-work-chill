c6a169bbf1c8744fccf9c7ffe2630e837616ab47
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-02-06 14:14:59 -0800

    styling for components

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import RegistrationPage from '../Registration/Registration';
-
+import "../Landing/Landing.css"
 
 // using class allows us to manage state
 class LandingPage extends Component {
@@ -13,11 +13,17 @@ class LandingPage extends Component {
                     <Route path="/register" component={RegistrationPage} />
                 </Router>
                 <div className="jumbotron">
-                        <h1 className="display-4">Live, Work, Chill</h1>
-                        <p className="lead">An app that checks rent prices and stuff...</p>
-                        <button><Link to="/register">Get Started</Link></button>
+                        <h1 className="display-4">Live Work Chill</h1>
+                        <p className="lead">An app that helps you find a home</p>
+                        <p className="lead1"> that's worthy of your hard work</p>
+                        <div className="button">
+                        <button ><Link className="button1"  to="/register">Get Started</Link></button>
+                        </div>
+    
                 </div>
             </div>
+            
+
 
         );
     }
